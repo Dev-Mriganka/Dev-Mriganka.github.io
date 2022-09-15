2afbd686d92fcf85b75c0a87e4c667771eefa9e9
Dev-Mriganka <mrigankamondal10@gmail.com>
2022-09-15 17:04:07 +0530

    Day-4

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector(".open-btn").addEventListener("click", function(){
+   document.querySelector(".mobileMenu").classList.toggle("show")
+   // console.log("hi");
+});
+document.querySelector(".menu-close").addEventListener("click", function () {
+   document.querySelector(".mobileMenu").classList.toggle("show")
+   // console.log("hi");
+});
+// openBtn()
